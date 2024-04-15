cb2973daeff040bc19f341ad58b435eaf2049c1e
Isaac Machakata <isaac@caasi.co.zw>
2024-04-15 20:44:07 +0200

    feat: add aut middleware

---
+++ app/Http/Middleware/UserIsLogged.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserIsLogged
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!session()->get('user')) {
+            return abort(401, 'Unauthorized!');
+        }
+        return $next($request);
+    }
+}
