9b5a024c7a353460a4e27fdc1b92401561e85987
Isaac Machakata <isaac@caasi.co.zw>
2024-04-15 14:58:56 +0200

    refactor: add login page

---
+++ resources/views/auth/signin.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+@section('title','Login')
+@section('content')
+<main>
+    <div class="container-fluid">
+        <div class="container my-5">
+            <h1 class="fs-1">Login to your account</h1>
+            <p class="lead">You need to be logged in to access the online library.</p>
+        </div>
+    </div>
+</main>
+@endSection()
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/search-books', [BooksController::class, 'index']);
+Route::get('/login', [AccountController::class, 'login']);
