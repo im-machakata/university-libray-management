86a1117c922157e5c217945df032f8e6e09b2f3a
Isaac Machakata <isaac@caasi.co.zw>
2024-04-15 21:17:29 +0200

    chore: add more columns to users tables

---
+++ database/migrations/2024_04_15_185040_add_course_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('course_id')->nullable()->after('password')->constrained();
+            $table->string('surname')->after('name');
+            $table->string('phone')->nullable()->after('surname');
+            $table->string('username')->after('phone')->unique();
+            $table->string('school_id')->nullable()->after('username')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('course_id');
+            $table->dropColumn(['surname', 'phone', 'username', 'school_id']);
+        });
+    }
+};
