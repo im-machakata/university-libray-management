b559fbea1b5702bd00de18366d25596d54479336
Isaac Machakata <isaac@caasi.co.zw>
2024-04-17 00:35:38 +0200

    refactor: pass tutorials to frontend

--- app/Http/Controllers/TutorialsController.php
+++ app/Http/Controllers/TutorialsController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tutorial;
 use Illuminate\Http\Request;
 
 class TutorialsController extends Controller
 {
     public function index()
     {
-        return view('tutorials/index');
+        $tutorials = Tutorial::query()->paginate();
+        return view('tutorials/index')->with('tutorials', $tutorials);
     }
 }
