87b087ca191384235df6050f8158415fee02b22a
Isaac Machakata <isaac@caasi.co.zw>
2024-04-16 06:39:06 +0200

    refactor: add more fillable fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'username',
+        'phone',
+        'school_id',
         'email',
         'password',
     ];
