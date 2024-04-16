148742202eb62079f6aeb7b6455e70082bf5eb93
Isaac Machakata <isaac@caasi.co.zw>
2024-04-17 00:45:01 +0200

    refactor: remove images on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <main>
     <div class="container-fluid">
         <div class="container my-5">
-            <h1 class="fs-1">Great Zimbabwe University Online Library</h1>
+            <h1 class="fs-2">Great Zimbabwe University Online Library</h1>
             <p class="lead">Search and read text books from the comfort of your home with our online library.</p>
         </div>
     </div>
@@ -13,41 +13,37 @@
             <div class="row justify-content-between">
                 <div class="col-md-4 col-lg-3">
                     <div class="card rounded-3 mb-3">
-                        <img src="/static/images/class.jpg" class="card-img-top rounded-top-3" alt="...">
                         <div class="card-body">
-                            <h2 class="h4 card-title">Chat with us</h2>
+                            <h2 class="fs-4 card-title">Chat with us</h2>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/chat-with-a-librarian" class="btn btn-warning d-block text-uppercase">Chat</a>
+                            <a href="/chat-with-a-librarian" class="btn btn-outline-success d-block text-uppercase">Chat</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 col-lg-3">
                     <div class="card rounded-3 mb-3">
-                        <img src="/static/images/class.jpg" class="card-img-top rounded-top-3" alt="...">
                         <div class="card-body">
-                            <h2 class="h4 card-title">Read Books</h2>
+                            <h2 class="fs-4 card-title">Read Books</h2>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/chat-with-a-librarian" class="btn btn-warning d-block text-uppercase">Read books</a>
+                            <a href="/chat-with-a-librarian" class="btn btn-outline-success d-block text-uppercase">Read books</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 col-lg-3">
                     <div class="card rounded-3 mb-3">
-                        <img src="/static/images/class.jpg" class="card-img-top rounded-top-3" alt="...">
                         <div class="card-body">
-                            <h2 class="h4 card-title">System Tutorials</h2>
+                            <h2 class="fs-4 card-title">System Tutorials</h2>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/chat-with-a-librarian" class="btn btn-warning d-block text-uppercase">View tutorials</a>
+                            <a href="/chat-with-a-librarian" class="btn btn-outline-success d-block text-uppercase">View tutorials</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 col-lg-3">
                     <div class="card rounded-3 mb-3">
-                        <img src="/static/images/class.jpg" class="card-img-top rounded-top-3" alt="...">
                         <div class="card-body">
-                            <h2 class="h4 card-title">Create Account</h2>
+                            <h2 class="fs-4 card-title">Create Account</h2>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/register" class="btn btn-warning d-block text-uppercase">Register</a>
+                            <a href="/register" class="btn btn-outline-success d-block text-uppercase">Register</a>
                         </div>
                     </div>
                 </div>
