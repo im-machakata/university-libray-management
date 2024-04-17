7435762221b3dd3b32e628a464a184f33a9df09d
Isaac Machakata <isaac@caasi.co.zw>
2024-04-17 19:50:25 +0200

    refactor: add chats to view

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chat;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
     public function index(Request $request)
     {
-        return view('chat/index');
+        $user = auth()->user();
+        $chats = Chat::query()->where('sender_id', $user);
+        return view('chat/index')
+            ->with('chats', $chats);
     }
 }
