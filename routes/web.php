9fb21b9b09b98c5ebad60f674ff9ff5280cbb853
Isaac Machakata <isaac@caasi.co.zw>
2024-04-30 03:48:37 +0200

    refactor: register new authors urls

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\ChatController;
@@ -38,7 +39,7 @@ Route::middleware(['auth', 'session.valid'])->group(static function () {
     // add all routes that need to be accessed while logged in here. 
     Route::get('/logout', [UsersController::class, 'delete']);
     Route::get('/search-books', [BooksController::class, 'index']);
-    Route::get('/read-books', [BooksController::class, 'read']);
+    Route::get('/read-books', [BooksController::class, 'read'])->name('books.read');
     Route::get('/talk-to-a-librarian', [ChatController::class, 'index']);
 });
 
@@ -46,6 +47,8 @@ Route::middleware(['auth', 'session.valid', UserIsAdmin::class])->group(static f
     // add all routes that need to be accessed with admins
     Route::get('/books/new', [BooksController::class, 'create'])->name('books.create');
     Route::post('/books/new', [BooksController::class, 'store'])->name('books.store');
+    Route::get('/authors/new', [AuthorsController::class, 'create'])->name('authors.create');
+    Route::post('/authors/new', [AuthorsController::class, 'store'])->name('authors.store');
     Route::get('/tutorials/new', [TutorialsController::class, 'create'])->name('tutorials.create');
     Route::post('/tutorials/new', [TutorialsController::class, 'store'])->name('tutorials.store');
 });
