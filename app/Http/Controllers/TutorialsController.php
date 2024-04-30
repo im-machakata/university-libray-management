cb5a681d1d3c3c430d2647364ad2e0f395fe7d9b
Isaac Machakata <isaac@caasi.co.zw>
2024-04-30 03:31:21 +0200

    feat: allow saving and viewing new tutorials

--- app/Http/Controllers/TutorialsController.php
+++ app/Http/Controllers/TutorialsController.php
@@ -17,4 +17,18 @@ class TutorialsController extends Controller
         $tutorial = Tutorial::query()->whereSlug($slug)->firstOrFail();
         return view('tutorials/read')->with('tutorial', $tutorial);
     }
+    public function create()
+    {
+        return view('tutorials/create');
+    }
+    public function store(Request $request)
+    {
+        $tutorialDetails = $request->validate([
+            'title' => ['required', 'min:3', 'max:255'],
+            'slug'=> ['required','min:3','max:255','unique:tutorials'],
+            'content' => ['required','min:10','max:10000'],
+        ]);
+        $tutorial = Tutorial::query()->create($tutorialDetails);
+        return redirect()->route('tutorials.read', ['slug' => $tutorial->slug]);
+    }
 }
